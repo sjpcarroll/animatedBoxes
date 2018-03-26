04863d05e43a914c433c0c9e24fa4c5749399dbc
Shaun Carroll <sjpcarroll@gmail.com>
2018-03-26 16:18:59 +1000

    initial push

---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="nimbleSquare web design comming soon">
+    <meta name="author" content="Shaun Carroll">
+    <meta name="description" content="Comming soon, Your favourite Brisbane based web design company"/>
+    <meta name=”robots” content=”index, nofollow”/>
+    <meta name="geo.region" content="AU"/>
+    <meta name="geo.position" content="-25.274398;133.775136"/>
+    <meta name="ICBM" content="-25.274398, 133.775136"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>nimbleSquare</title>
+    <!-- Favicon -->
+    <link rel="icon" href="">
+    <!-- Bootstrap 4 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assests/css/main.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <style>
+        .content {
+            margin-top : 50px;
+        }
+
+    </style>
+
+</head>
