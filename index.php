003ae97dea73f767894f3882c223647e2d9cfb0a
Shaun Carroll <sjpcarroll@gmail.com>
2018-04-03 18:47:10 +1000

    cleaning up some code and adding of favicons

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include_once('templates/header.php'); ?>
-<?php include_once('templates/topNavBar.php'); ?>
-  <div class="container">
-
-  </div>
-<?php include_once('templates/footer.php'); ?>
